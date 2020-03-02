bef67fbd91d7b6602fccc4bb910848e63c37cd47
Randolph Ledesma <me@randolphledesma.com>
2020-03-02 02:08:58 -0500

    add codes for learning Kotlin programming

---
+++ kotlin/basics.kt
@@ -0,0 +1,24 @@
+package com.randolphledesma
+
+fun main(args: Array<String>) {
+    println("Hello World!")
+
+    //Single line comment
+
+    /* Multi line
+        comment
+     */
+
+    val number: Int = 0 //Immutable integer variable
+
+    var name: String = "" //Mutable string variable
+    name = "Randolph Ledesma"
+
+    /*
+    Arithmetic operations
+     */
+    println(1 + 1)
+    println(3 - 2)
+    println(3 * 4)
+    println(12 / 6)
+}
