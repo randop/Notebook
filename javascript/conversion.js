128e6db5c9f9265156ed5a969ab1e2760770bef8
Randolph Ledesma <me@randolphledesma.com>
2020-03-26 07:30:35 -0400

    updates

---
+++ javascript/conversion.js
@@ -0,0 +1,14 @@
+/**
+ * Here is a prototype to convert a number to a readable string respecting the new international standards.
+ * https://stackoverflow.com/questions/10420352/converting-file-size-in-bytes-to-human-readable-string/10420404#answer-20463021
+ */
+
+Object.defineProperty(Number.prototype,'fileSize',{value:function(a,b,c,d){
+    return (a=a?[1e3,'k','B']:[1024,'K','iB'],b=Math,c=b.log,
+    d=c(this)/c(a[0])|0,this/b.pow(a[0],d)).toFixed(2)
+    +' '+(d?(a[1]+'MGTPEZY')[--d]+a[2]:'Bytes');
+   },writable:false,enumerable:false});
+
+console.log((186457865).fileSize()); // default IEC (power 1024)
+//177.82 MiB
+//KiB,MiB,GiB,TiB,PiB,EiB,ZiB,YiB
