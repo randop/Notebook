88e22cd07922aa55e61350cc7423d7ad9ca3eabe
Randolph <randop@me.com>
2020-04-14 20:39:20 +0800

    Kotlin example of Extension Function Expressions

--- kotlin/efe.kt
+++ kotlin/efe.kt
@@ -6,6 +6,12 @@ fun main(args: Array<String>) {
     db.inTransaction {
         delete("users", "first_name = ?", arrayOf("Ran"))
     }
+
+    preferences.edit {
+        putString("foo", "bar")
+        putString("fizz", "buzz")
+        remove("username")
+    }
 }
 
 inline fun SQLiteDatabase.inTransaction(func: SQLiteDatabase.() -> Unit) {
@@ -16,4 +22,10 @@ inline fun SQLiteDatabase.inTransaction(func: SQLiteDatabase.() -> Unit) {
     } finally {
         endTransaction()
     }
+}
+
+inline fun SharedPreferences.edit(func: SharedPreferences.Editor.() -> Unit) {
+    val editor = edit()
+    editor.func()
+    editor.apply()
 }
