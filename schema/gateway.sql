c52f3e9d21c86d45a6edc25499e2d2d312fa1899
Randolph <randop@me.com>
2020-04-23 14:02:36 +0800

    Jude 1:20-25

--- schema/gateway.sql
+++ schema/gateway.sql
@@ -1,3 +1,8 @@
 CREATE DATABASE gateway
   CHARACTER SET utf8mb4
   COLLATE utf8mb4_unicode_ci;
+
+ALTER USER 'root'@'localhost' IDENTIFIED BY 'RANDOM-PASSWORD-VukeJ@LdUseLqyMyhuklugc3qic';
+
+CREATE USER 'gateway'@'%' IDENTIFIED BY 'password';
+GRANT ALL PRIVILEGES ON gateway.* TO 'gateway'@'%';
