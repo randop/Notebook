3123d4455eef0291a5be9dee02be6c0d4d6f2e50
Randolph <randop@me.com>
2022-04-28 08:08:53 +0000

    Improves javascript obliterate annoying website elements

--- javascript/obliterate.js
+++ javascript/obliterate.js
@@ -2,18 +2,24 @@
  * Run this script on your browsers console to obliterate annoying signup wall and blurring on websites 
  */
 
-javascript: (function() {
-    (function() {
-        var i, elements = document.querySelectorAll('body *');
-        for (i = 0; i < elements.length; i++) {
-            if (getComputedStyle(elements[i]).position === 'fixed') {
-                elements[i].parentNode.removeChild(elements[i]);
-            }
-            if (getComputedStyle(elements[i]).filter.indexOf("blur")==0) {
-                elements[i].style.setProperty('filter', 'none', 'important')
-            }
+function obliterate() {
+    var i, elements = document.querySelectorAll('body *');
+    for (i = 0; i < elements.length; i++) {
+        if (getComputedStyle(elements[i]).position === 'fixed') {
+            elements[i].parentNode.removeChild(elements[i]);
         }
-    })();
+        if (getComputedStyle(elements[i]).filter.indexOf("blur")==0) {
+            elements[i].style.setProperty('filter', 'none', 'important')
+        }
+    }
     document.querySelector('body').style.setProperty('overflow', 'auto', 'important');
     document.querySelector('html').style.setProperty('overflow', 'auto', 'important');
-})();
+}
+
+setInterval(function obliterateTimer() {
+    obliterate();
+}, 500);
+setTimeout(function obliterateRun() {
+    obliterate();
+},1);
+
